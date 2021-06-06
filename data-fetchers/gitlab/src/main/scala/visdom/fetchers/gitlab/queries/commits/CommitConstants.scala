a1db75404003dad2802352f0b0d406b433d0f052
Ville Heikkilä <ville.heikkila@tuni.fi>
2021-06-07 00:35:30 +0300

    Fixed docker-compose port - also fixed query example to match implementation - https://trello.com/c/ejktZRMQ

--- data-fetchers/gitlab/src/main/scala/visdom/fetchers/gitlab/queries/commits/CommitConstants.scala
+++ data-fetchers/gitlab/src/main/scala/visdom/fetchers/gitlab/queries/commits/CommitConstants.scala
@@ -67,9 +67,9 @@ object CommitConstants {
             "reference": "master",
             "startDate": "2020-01-01T00:00:00.000Z",
             "endDate": "2021-01-01T00:00:00.000Z",
-            "includeStatistics": true,
-            "includeFileLinks": true,
-            "includeReferenceLinks": false
+            "includeStatistics": "true",
+            "includeFileLinks": "true",
+            "includeReferenceLinks": "false"
         }
     }"""
 
