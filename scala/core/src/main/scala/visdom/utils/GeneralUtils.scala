ddadf2e41c56b4f002fc9a35bc0228d9545af44b
Ville Heikkilä <ville.heikkila@tuni.fi>
2021-09-19 22:21:11 +0300

    Salt added for the hashing function - https://trello.com/c/DltWPKEs

--- scala/core/src/main/scala/visdom/utils/GeneralUtils.scala
+++ scala/core/src/main/scala/visdom/utils/GeneralUtils.scala
@@ -144,9 +144,10 @@ object GeneralUtils {
     val ShaFunction: String = "SHA-512/256"
     val Encoding: String = "UTF-8"
     val MessageDigester: MessageDigest = MessageDigest.getInstance(ShaFunction)
+    val secretWord: String = EnvironmentVariables.getEnvironmentVariable(EnvironmentVariables.EnvironmentSecretWord)
 
     def getHash(inputString: String): String = {
-        val digest = MessageDigester.digest(inputString.getBytes(Encoding))
+        val digest = MessageDigester.digest((secretWord + inputString).getBytes(Encoding))
         String.format(s"%0${digest.length * 2}x", new BigInteger(1, digest))
     }
 
