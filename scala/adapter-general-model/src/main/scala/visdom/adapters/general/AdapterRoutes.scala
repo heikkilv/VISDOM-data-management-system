c09214e7f3a963fa374f28701291f002766044fe
Ville Heikkilä <ville.heikkila@tuni.fi>
2022-02-14 17:45:43 +0200

    Added single item query to the routes.

--- scala/adapter-general-model/src/main/scala/visdom/adapters/general/AdapterRoutes.scala
+++ scala/adapter-general-model/src/main/scala/visdom/adapters/general/AdapterRoutes.scala
@@ -4,8 +4,10 @@ import akka.actor.Props
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.Route
 import visdom.http.server.actors.GeneralAdapterInfoActor
+import visdom.http.server.actors.SingleActor
 import visdom.http.server.actors.TestActor
 import visdom.http.server.services.AdapterInfoService
+import visdom.http.server.services.SingleService
 import visdom.http.server.services.TestService
 import visdom.http.server.swagger.SwaggerAdapterDocService
 import visdom.http.server.swagger.SwaggerRoutes
@@ -14,12 +16,14 @@ import visdom.http.server.swagger.SwaggerRoutes
 object AdapterRoutes extends visdom.adapters.AdapterRoutes {
     override val routes: Route = Directives.concat(
         new AdapterInfoService(system.actorOf(Props[GeneralAdapterInfoActor])).route,
+        new SingleService(system.actorOf(Props[SingleActor])).route,
         new TestService(system.actorOf(Props[TestActor])).route,
         SwaggerRoutes.getSwaggerRouter(
             new SwaggerAdapterDocService(
                 Adapter.adapterValues,
                 Set(
                     classOf[AdapterInfoService],
+                    classOf[SingleService],
                     classOf[TestService]
                 )
             )
