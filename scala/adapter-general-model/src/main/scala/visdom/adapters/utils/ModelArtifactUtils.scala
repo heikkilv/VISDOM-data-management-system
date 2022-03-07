94d3085d3a2d518faa553c80142d0e60af2bf557
Ville Heikkilä <ville.heikkila@tuni.fi>
2022-03-07 18:20:30 +0200

    Folder contents added as file links to parent folder artifacts.

--- scala/adapter-general-model/src/main/scala/visdom/adapters/utils/ModelArtifactUtils.scala
+++ scala/adapter-general-model/src/main/scala/visdom/adapters/utils/ModelArtifactUtils.scala
@@ -9,15 +9,47 @@ import visdom.adapters.general.schemas.FileSchema
 import visdom.adapters.general.schemas.PipelineReportSchema
 import visdom.database.mongodb.MongoConstants
 import visdom.utils.CommonConstants
+import visdom.utils.GeneralUtils
+import visdom.utils.SnakeCaseConstants
 
 
 class ModelArtifactUtils(sparkSession: SparkSession, modelUtils: ModelUtils) {
     import sparkSession.implicits.newProductEncoder
+    import sparkSession.implicits.newSequenceEncoder
+    import sparkSession.implicits.newStringEncoder
 
     def getFiles(): Dataset[FileArtifactResult] = {
-        modelUtils.loadMongoData[FileSchema](MongoConstants.CollectionFiles)
-            .flatMap(row => FileSchema.fromRow(row))
-            .map(fileSchema => ArtifactResult.fromFileSchema(fileSchema))
+        val allFiles: Dataset[FileSchema] =
+            modelUtils.loadMongoData[FileSchema](MongoConstants.CollectionFiles)
+                .flatMap(row => FileSchema.fromRow(row))
+
+        val fileParentMap: Map[(String, String, String), String] =
+            allFiles
+                .filter(fileSchema => fileSchema.`type` == SnakeCaseConstants.Blob)
+                .map(
+                    fileSchema => (
+                        fileSchema.host_name,
+                        fileSchema.project_name,
+                        fileSchema.path,
+                        GeneralUtils.getUpperFolder(fileSchema.path)
+                    )
+                )
+                .groupByKey({case (hostName, projectName, _, upperFolder) => (hostName, projectName, upperFolder)})
+                .mapValues({case (_, _, path, _) => Seq(path)})
+                .reduceGroups((first, second) => first ++ second)
+                .collect()
+                .toMap
+
+        allFiles
+            .map(
+                fileSchema => ArtifactResult.fromFileSchema(
+                    fileSchema,
+                    fileSchema.`type` == SnakeCaseConstants.Tree match {
+                        case true => fileParentMap.getOrElse((fileSchema.host_name, fileSchema.project_name, fileSchema.path), Seq.empty)
+                        case false => Seq.empty
+                    }
+                )
+            )
     }
 
     def getPipelineReports(): Dataset[PipelineReportArtifactResult] = {
