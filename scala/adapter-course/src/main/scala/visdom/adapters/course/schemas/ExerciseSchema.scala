68f281d886d6f83a221f7f463054f5ee9d1177cc
Ville Heikkilä <ville.heikkila@tuni.fi>
2022-06-30 15:46:56 +0300

    Add license text to Scala source code

--- scala/adapter-course/src/main/scala/visdom/adapters/course/schemas/ExerciseSchema.scala
+++ scala/adapter-course/src/main/scala/visdom/adapters/course/schemas/ExerciseSchema.scala
@@ -1,3 +1,8 @@
+// Copyright 2022 Tampere University
+// This software was developed as a part of the VISDOM project: https://iteavisdom.org/
+// This source code is licensed under the MIT license. See LICENSE in the repository root directory.
+// Author(s): Ville Heikkilä <ville.heikkila@tuni.fi>
+
 package visdom.adapters.course.schemas
 
 import visdom.spark.FieldDataType
--- scala/adapter-general-model/src/main/scala/visdom/adapters/utils/CourseUtils.scala
+++ scala/adapter-general-model/src/main/scala/visdom/adapters/utils/CourseUtils.scala
@@ -1,3 +1,8 @@
+// Copyright 2022 Tampere University
+// This software was developed as a part of the VISDOM project: https://iteavisdom.org/
+// This source code is licensed under the MIT license. See LICENSE in the repository root directory.
+// Author(s): Ville Heikkilä <ville.heikkila@tuni.fi>
+
 // Note, this code is copied from the course adapter
 package visdom.adapters.utils
 
